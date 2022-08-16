486495f674b7ae990da89dcc026b12da37872806
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-16 08:46:46 +0530

    setting the department - Done

--- emailApp/Email.java
+++ emailApp/Email.java
@@ -1,5 +1,8 @@
 package emailApp;
 
+// import used to get import
+import java.util.Scanner;
+
 public class Email {
 
     // encapsulate it because we don't want people to access our variables
@@ -11,8 +14,47 @@ public class Email {
     private String alternateEmail;
 
     // constructor to receive first name and the lastname
+    public Email(String firstName, String lastName){
+
+        // variable assigning after getting the first name and the last name
+        // this refers to the class level
+        this.fName = firstName;
+        this.lName = lastName;
+
+        System.out.println("Email made for " + this.fName + " " +this.lName);
+
+        // after taking the names call a method to get the department
+        this.department = setDepartment();
+
+        System.out.println("The department is: " + this.department );
+    }
 
     // ask for the department
+    // if we return a string from the whole function then we set the method to string
+    // if we return nothing then we can use void
+    private String setDepartment() {
+        System.out.print("Department codes:  \n1 for Sales \n2 for Development \n3 for Accounting \n0 for None \nEnter the department code :");
+
+        // getting user input
+        Scanner input = new Scanner(System.in);
+
+        // since the user is inputting a integer we can use nextInt else if string we can use nextLine
+        int choiceDepartment = input.nextInt();
+
+        // validating the user input for a division
+        if (choiceDepartment == 1){
+            return "Sales";
+        }
+        else if (choiceDepartment == 2){
+            return "Development";
+        }
+        else if (choiceDepartment == 3){
+            return "Accounting";
+        }
+        else{
+            return "None";
+        }
+    }
 
     // generate a random password
 
--- emailApp/EmailApp.java
+++ emailApp/EmailApp.java
@@ -3,5 +3,7 @@ package emailApp;
 public class EmailApp {
     public static void main(String[] args) {
 
+        // calling the object from email and then declaring the parameters for the constructor
+        Email email1 = new Email("Yehan", "Yeshminda");
     }
 }
