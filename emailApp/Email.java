a2ceffc3027a6e3f6649acc5ca1cfb9d597c1b34
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-08-16 08:08:02 +0530

    created the blue print for the email application

---
+++ emailApp/Email.java
@@ -0,0 +1,25 @@
+package emailApp;
+
+public class Email {
+
+    // encapsulate it because we don't want people to access our variables
+    private String fName;
+    private String lName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    // constructor to receive first name and the lastname
+
+    // ask for the department
+
+    // generate a random password
+
+    // set the mailbox capacity
+
+    // set the alternate email
+
+    // change the password
+
+}
